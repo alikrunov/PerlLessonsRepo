89a8ebbce4052137daa20c31bf2d3f51baa60be5
alikrunov <alikrunov@gmail.com>
2024-02-29 17:50:32 +0300

    Create json parser

--- t/Parser.pl
+++ t/Parser.pl
@@ -3,10 +3,28 @@ use strict;
 use warnings;
 
 use XML::LibXML;
+use JSON;
 
-my $filename = 'C:\Users\EVGENIA\lessons\PerlLessons\simple.xml';
+my $xml_filename = 'C:\Users\EVGENIA\lessons\PerlLessons\simple.xml';
 
-my $doc = XML::LibXML -> load_xml( location => $filename );
+my $doc = XML::LibXML -> load_xml( location => $xml_filename );
 for my $node ( $doc -> findnodes( '//text()' ) ) {
     print $node -> parentNode() -> nodeName(), ": ", $node -> nodeValue(), "\n";
 }
+
+my $json_filename = 'C:\Users\EVGENIA\lessons\PerlLessons\simple.json';
+
+my $json_text = do {
+    open( my $json_fh, "<:encoding(UTF-8)", $json_filename )
+        or die( "Can't open \"$json_filename\": $!\n" );
+    local $/;
+    <$json_fh>
+};
+
+my $json = JSON -> new;
+my $data = $json -> decode( $json_text );
+
+for my $string ( keys %$data ) {
+    print( $string . '=' . %$data{$string} . "\n" );
+}
+
